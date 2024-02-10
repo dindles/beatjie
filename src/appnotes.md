9646783d22337bff312332f9b3de2def5cf1deef
dindles <114931816+dindles@users.noreply.github.com>
2024-02-10 12:45:04 +0200

    Piecing together a Sample class

--- src/appnotes.md
+++ src/appnotes.md
@@ -1,8 +1,37 @@
 ## todo
 
+<!-- todo next -->
+
+get Sample class working
+
+- make empty loaded_samples array
+- loop through packs and sample headers,
+- and for each one:
+  - firstly, as proof of concept
+  - initialise Sample object using constructor
+    - that means assigning all sampler, env, filter, panner, analyser objects
+    - chaining them
+  -
+
+use play method to trigger sample
+
+OR
+
+- get chooseSample working
+- get playSample working through sampler
+
+<!-- todo -->
+
+Get sequence working.
+
+- try sequence as Sample attribute
+- Replicate Skunkworks, not using onMount so far as possible, and simplifying.
+- Choose sample, have it load correct sequence
+- try Sample class: initialise, play, dispose methods
+
 - check sample urls
 - choose emojis
 
 # issues
 
-grids and mojis don't behave well when zoomed
+grids and mojis don't behave well when zoomed – use rem for tile size?
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -14,12 +14,53 @@ export type PackHeader = {
 export type Pack = SampleHeader[]
 export type Packs = PackHeader[]
 
-export type Sample = SampleHeader & {
+// export type Sample = SampleHeader & {
+//   sampler: Tone.Sampler
+//   envelope: Tone.AmplitudeEnvelope
+//   filter: Tone.Filter
+//   panner: Tone.Panner
+//   sequence: Sequence
+// }
+
+export class Sample<Sample extends SampleHeader> {
+  id: number
+  name: string
+  emoji: string
+  url: string
   sampler: Tone.Sampler
   envelope: Tone.AmplitudeEnvelope
   filter: Tone.Filter
   panner: Tone.Panner
+  sequence: Sequence
+  constructor(id: number, name: string, emoji: string, url: string) {
+    this.id = id
+    this.name = name
+    this.emoji = emoji
+    this.url = url
+    this.sampler = new Tone.Sampler({
+      urls: {
+        C2: `${this.url}`,
+      },
+      baseUrl: 'src/lib/assets/audio/',
+      onload: () => {
+        console.log('loaded:' + this.url)
+      },
+    })
+    this.envelope = new Tone.AmplitudeEnvelope({
+      attack: 0.01,
+      decay: 0.1,
+      sustain: 0.5,
+      release: 0.5,
+    })
+    this.filter = new Tone.Filter({
+      type: 'lowpass',
+      frequency: 2000,
+    })
+    this.panner = new Tone.Panner()
+    // todo - this sequence length should refer to a global store
+    this.sequence = { sample_id: this.id, steps: new Array(16).fill(false) }
+  }
 }
 
-export type Sequence = boolean[]
-export type Sequences = [boolean[][]]
+export type Sequence = { sample_id: number; steps: boolean[] }
+export type Sequences = [Sequence[]]
