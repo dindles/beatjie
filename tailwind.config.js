e24ed904b98967c31b114d0d860bcf58ee075349
dindles <114931816+dindles@users.noreply.github.com>
2024-06-30 13:36:31 +0200

    preTWgridification

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        20: 'repeat(20, 1fr) ',
+      },
+    },
   },
   plugins: [],
 }
